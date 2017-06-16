ad3042a2027eea6b4e3417ce8d93e7c51a0b143c
aidenglaspey <aiden.glaspey@Gmail.com>
2017-06-16 05:58:14 -0700

    Add files via upload

---
+++ discord.js
@@ -0,0 +1,37 @@
+const embed = {
+  "title": "Guys, this is pretty cool.",
+  "description": "So, I can write a really neat 'embed' in Discord which ends up looking like this. The link will take you to the site.",
+  "url": "https://leovoel.github.io/embed-visualizer/",
+  "color": 16711680,
+  "timestamp": "1998-03-06T07:43:12.485Z",
+  "footer": {
+    "icon_url": "https://emojipedia-us.s3.amazonaws.com/cache/85/d1/85d1ef7f32e4fa4bcdd5b2a6ed5c71d6.png",
+    "text": "This Date is Fake"
+  },
+  "thumbnail": {
+    "url": "https://images-ext-2.discordapp.net/external/3iBDBlHIIBBlGlJ9QeP76R-4s3TsR_4LztIxVZmfoi4/https/cdn2.iconfinder.com/data/icons/picons-basic-2/57/basic2-031_hi_bubble_welcome-512.png?width=80&height=80"
+  },
+  "image": {
+    "url": "http://www.reactiongifs.com/r/2013/11/yump.gif"
+  },
+  "author": {
+    "name": "Aiden Glaspey",
+    "url": "mailto:aiden.glaspey@gmail.com",
+    "icon_url": "https://cdn.discordapp.com/avatars/231187031261184000/55f5b2a336f799a857781fa9434477e3.png"
+  },
+  "fields": [
+    {
+      "name": "I think you should give it a try.",
+      "value": "However, you'll need to know how to write in Javascript, or at least navigate around it. Let's make a list."
+    },
+    {
+      "name": "Rule 1:",
+      "value": "Memes plz."
+    },
+    {
+      "name": "Conclusion",
+      "value": "This is pretty damned cool, man. I am glad I looked into this. Hopefully I didn't just waste my time making this and I can actually embed it. "
+    }
+  ]
+};
+channel.send({ embed });
